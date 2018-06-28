4d167a6330255000228ff9ef5601d0b3e08f1ddf
krishhpatell <krishhpatell@krishs-mbp.fios-router.home>
2018-06-28 09:21:25 -0400

    Intial Commit

--- batch27new/src/Operator/Incrementexample.java
+++ batch27new/src/Operator/Incrementexample.java
@@ -4,19 +4,27 @@ public class Incrementexample {
 	
 public static void main(String[] args) {
  int year = 2018;
+ //System.out.println(year);
 // System.out.println(year);
 // year = year+1;
 // System.out.println(year);
 // year+= 1;
 // System.out.println(year);
 // year++;
-// System.out.println(year);
-//
 
-System.out.println(year++);
-System.out.println(year++);
+ 
+System.out.println(year);
+System.out.println(++year);
+System.out.println(year);
+System.out.println(year);
 System.out.println(year++);
 System.out.println(year);
- 
+System.out.println(year);
+System.out.println(year++);
+System.out.println(year);
+System.out.println(year++);
+System.out.println(year);
+System.out.println(++year);
+
 }
 }
